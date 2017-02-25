1262e91b22161936521feb2faa40aed428113ee9
Amber Febbraro <amber.febbraro@gmail.com>
2017-02-24 22:25:49 -0500

    remove host name

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var http = require("http");
 var port = process.env.PORT || 1337; // Sets the port to whatever the env var is, if there isn't one then set to 1337
-var hostname = '127.0.0.1';
 
 http.createServer(function (request, response) {
     response.end('Hello World!');
-}).listen(port, hostname, function() {
-    console.log("Server running at http://" + hostname + ':' + port + '/');
+}).listen(port, function() {
+    console.log("Server running at http://sitename" + ':' + port + '/');
 });
